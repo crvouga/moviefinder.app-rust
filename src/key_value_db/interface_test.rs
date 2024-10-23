7913f7faf11c780269f8a2b966c5822eaa853681
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2024-10-23 13:49:52 -0700

    Refactor code to add unit test for updating existing items in key-value database

--- src/key_value_db/interface_test.rs
+++ src/key_value_db/interface_test.rs
@@ -51,4 +51,38 @@ mod tests {
             assert_eq!(after, Ok(Some(item_serialized)));
         }
     }
+
+    #[tokio::test]
+    async fn test_updating_existing() {
+        for mut f in fixtures() {
+            let item = Item::random();
+            let item_serialized = serde_json::to_string(&item).unwrap_or("".to_string());
+
+            let before = f.key_value_db.get(&item.id).await;
+            let put = f.key_value_db.put(&item.id, item_serialized.clone()).await;
+            let after = f.key_value_db.get(&item.id).await;
+
+            assert_eq!(before, Ok(None));
+            assert_eq!(put, Ok(()));
+            assert_eq!(after, Ok(Some(item_serialized.clone())));
+
+            let updated_item = Item {
+                id: item.id.clone(),
+                value: "Updated value".to_string(),
+            };
+            let updated_item_serialized =
+                serde_json::to_string(&updated_item).unwrap_or("".to_string());
+
+            let before_update = f.key_value_db.get(&updated_item.id).await;
+            let put_update = f
+                .key_value_db
+                .put(&updated_item.id, updated_item_serialized.clone())
+                .await;
+            let after_update = f.key_value_db.get(&updated_item.id).await;
+
+            assert_eq!(before_update, Ok(Some(item_serialized)));
+            assert_eq!(put_update, Ok(()));
+            assert_eq!(after_update, Ok(Some(updated_item_serialized)));
+        }
+    }
 }
