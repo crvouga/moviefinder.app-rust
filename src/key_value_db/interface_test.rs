e6cc4279e3c62416cc8b2e1585de6404628ab9bd
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2024-10-23 14:09:01 -0700

    Refactor code to remove unused code and improve code organization in interface_test.rs

--- src/key_value_db/interface_test.rs
+++ src/key_value_db/interface_test.rs
@@ -57,21 +57,16 @@ mod tests {
         for mut f in fixtures() {
             let item = Item::random();
             let item_serialized = serde_json::to_string(&item).unwrap_or("".to_string());
-
-            let before = f.key_value_db.get(&item.id).await;
-            let put = f.key_value_db.put(&item.id, item_serialized.clone()).await;
-            let after = f.key_value_db.get(&item.id).await;
-
-            assert_eq!(before, Ok(None));
-            assert_eq!(put, Ok(()));
-            assert_eq!(after, Ok(Some(item_serialized.clone())));
-
             let updated_item = Item {
                 id: item.id.clone(),
                 value: "Updated value".to_string(),
             };
             let updated_item_serialized =
                 serde_json::to_string(&updated_item).unwrap_or("".to_string());
+
+            let before = f.key_value_db.get(&item.id).await;
+            let put = f.key_value_db.put(&item.id, item_serialized.clone()).await;
+            let after = f.key_value_db.get(&item.id).await;
 
             let before_update = f.key_value_db.get(&updated_item.id).await;
             let put_update = f
@@ -80,6 +75,9 @@ mod tests {
                 .await;
             let after_update = f.key_value_db.get(&updated_item.id).await;
 
+            assert_eq!(before, Ok(None));
+            assert_eq!(put, Ok(()));
+            assert_eq!(after, Ok(Some(item_serialized.clone())));
             assert_eq!(before_update, Ok(Some(item_serialized)));
             assert_eq!(put_update, Ok(()));
             assert_eq!(after_update, Ok(Some(updated_item_serialized)));
@@ -105,6 +103,37 @@ mod tests {
 
             assert_eq!(zap, Ok(()));
             assert_eq!(after_zap, Ok(None));
+        }
+    }
+
+    #[tokio::test]
+    async fn test_child() {
+        for mut f in fixtures() {
+            let item = Item::random();
+            let item_serialized = serde_json::to_string(&item).unwrap_or("".to_string());
+
+            let before = f.key_value_db.get(&item.id).await;
+            let put = f.key_value_db.put(&item.id, item_serialized.clone()).await;
+            let after = f.key_value_db.get(&item.id).await;
+
+            assert_eq!(before, Ok(None));
+            assert_eq!(put, Ok(()));
+            assert_eq!(after, Ok(Some(item_serialized.clone())));
+
+            let mut child = f.key_value_db.child(vec!["child".to_string()]);
+            let child_item = Item::random();
+            let child_item_serialized =
+                serde_json::to_string(&child_item).unwrap_or("".to_string());
+
+            let before_child = child.get(&child_item.id).await;
+            let put_child = child
+                .put(&child_item.id, child_item_serialized.clone())
+                .await;
+            let after_child = child.get(&child_item.id).await;
+
+            assert_eq!(before_child, Ok(None));
+            assert_eq!(put_child, Ok(()));
+            assert_eq!(after_child, Ok(Some(child_item_serialized)));
         }
     }
 }
