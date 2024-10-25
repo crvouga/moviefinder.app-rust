7d6929a68eb861a8894218f63b82f6f539370188
Chris Vouga <crvouga@gmail.com>
2024-10-25 06:25:39 -0700

    Refactor code to extract session cookie creation into a separate function

--- src/core/http/wrap_session_id.rs
+++ src/core/http/wrap_session_id.rs
@@ -9,6 +9,20 @@ use std::pin::Pin;
 
 const SESSION_COOKIE_NAME: &str = "session_id";
 
+fn new_session_cookie(session_id: &str) -> HttpResponseCookie {
+    HttpResponseCookie {
+        domain: None,
+        expires: Some("0".to_string()),
+        path: Some("/".to_string()),
+        http_only: true,
+        secure: true,
+        max_age: Some(31536000),
+        name: SESSION_COOKIE_NAME.to_string(),
+        value: session_id.to_string(),
+        same_site: Some(SameSite::Strict),
+    }
+}
+
 pub fn wrap_session_id<F, Fut>(
     respond: F,
 ) -> impl Fn(HttpRequest) -> Pin<Box<dyn Future<Output = HttpResponse> + Send>> + Clone
@@ -22,17 +36,7 @@ where
 
         let fut = respond(&session_id, http_request).map(move |mut http_response| {
             if maybe_session_id.is_none() {
-                let session_cookie = HttpResponseCookie {
-                    domain: None,
-                    expires: Some("0".to_string()),
-                    path: Some("/".to_string()),
-                    http_only: true,
-                    secure: true,
-                    max_age: Some(31536000),
-                    name: SESSION_COOKIE_NAME.to_string(),
-                    value: session_id.as_str().to_string(),
-                    same_site: Some(SameSite::Strict),
-                };
+                let session_cookie = new_session_cookie(&session_id);
 
                 http_response
                     .headers
