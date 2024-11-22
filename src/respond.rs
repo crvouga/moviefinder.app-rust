65985bade6c69a80ae98014cccbef3cc7449db81
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2024-11-22 14:41:06 -0700

    Update OutputCss route to use css response variant for improved handling

--- src/respond.rs
+++ src/respond.rs
@@ -18,7 +18,7 @@ pub async fn respond(ctx: &ctx::Ctx, req: &Req, route: &Route) -> Res {
 
         Route::RobotsTxt => Res::text("User-agent: *\nDisallow:"),
 
-        Route::OutputCss => Res::text(include_str!("./output.css")),
+        Route::OutputCss => Res::css(include_str!("./output.css")),
 
         Route::Unknown(_route) => {
             Res::redirect_window(Route::Feed(feed::route::Route::DefaultLoad).encode())
