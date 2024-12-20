069a9aa7ae11ec53f6e6498955db1ca25c1bbf2d
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2024-12-20 11:28:22 -0700

    Refactor view_slide_content method in FeedItem; extract logic to a separate function for improved readability

--- src/feed/feed_item.rs
+++ src/feed/feed_item.rs
@@ -24,39 +24,6 @@ impl FeedItem {
             FeedItem::Media { media, .. } => Some(media.id.clone()),
         }
     }
-
-    pub fn view_slide_content(self: &FeedItem) -> Elem {
-        match self {
-            FeedItem::Media {
-                media,
-                feed_index: _,
-            } => div()
-                .class("w-full h-full flex flex-col content-box")
-                .child(
-                    Image::new()
-                        .view()
-                        .class(
-                            "w-full flex-1 overflow-hidden object-cover pointer-cursor content-box border-none outline-none",
-                        )
-                        .tab_index(0)
-                        .role_button()
-                        .data_on(|b| {
-                            b.click().push_then_sse(
-                                &media::details::route::Route::MediaDetailsScreen {
-                                    media_id: media.id.clone(),
-                                }
-                                .url(),
-                            )
-                        })
-                        .aria_label("open media details")
-                        .src(media.poster.to_highest_res())
-                        .alt(media.title.as_str()),
-                )
-                .child(interaction_form::respond::view_interaction_form(
-                    &media.id, None,
-                )),
-        }
-    }
 }
 
 impl From<(Media, usize)> for FeedItem {
--- src/feed/feed_screen/respond.rs
+++ src/feed/feed_screen/respond.rs
@@ -1,5 +1,3 @@
-use std::cmp::max;
-
 use super::{
     super::{feed_::Feed, feed_id::FeedId, feed_item::FeedItem, feed_tags_form},
     route::Route,
@@ -18,13 +16,13 @@ use crate::{
     },
     ctx::Ctx,
     debug,
-    media::{interaction::interaction_form, media_id::MediaId},
+    media::{self, interaction::interaction_form, media_id::MediaId},
     req::Req,
     ui::{bottom_bar::BottomBar, route::AppRoute},
 };
+use std::cmp::max;
 
 pub const LIMIT: usize = 3;
-
 pub struct ViewModel {
     pub feed_id: FeedId,
     pub feed: Feed,
@@ -368,5 +366,38 @@ pub fn view_slide_bottom_empty() -> Elem {
 pub fn view_slide(feed_item: &FeedItem) -> Elem {
     view_slide_root()
         .attr("data-feed-index", &feed_item.to_feed_index().to_string())
-        .child(feed_item.view_slide_content())
+        .child(view_slide_content(feed_item))
+}
+
+pub fn view_slide_content(feed_item: &FeedItem) -> Elem {
+    match feed_item {
+        FeedItem::Media {
+            media,
+            feed_index: _,
+        } => div()
+            .class("w-full h-full flex flex-col content-box")
+            .child(
+                Image::new()
+                    .view()
+                    .class(
+                        "w-full flex-1 overflow-hidden object-cover pointer-cursor content-box border-none outline-none",
+                    )
+                    .tab_index(0)
+                    .role_button()
+                    .data_on(|b| {
+                        b.click().push_then_sse(
+                            &media::details::route::Route::MediaDetailsScreen {
+                                media_id: media.id.clone(),
+                            }
+                            .url(),
+                        )
+                    })
+                    .aria_label("open media details")
+                    .src(media.poster.to_highest_res())
+                    .alt(media.title.as_str()),
+            )
+            .child(interaction_form::respond::view_interaction_form(
+                &media.id, None,
+            )),
+    }
 }
